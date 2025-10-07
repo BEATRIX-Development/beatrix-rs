55bf0a00759a730c03040b4029e29ee960609e5c
Juniper Beatitudes <juniper@eyes-like-fire.org>
2025-10-07 11:32:49 -0500

    Initial commits

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              (rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+                targets = [ "thumbv7em-none-eabihf" ];
+              })
+            ]
+            ++ [
+              rust-analyzer
+              rustfmt
+            ];
+        };
+      }
+    );
+}
